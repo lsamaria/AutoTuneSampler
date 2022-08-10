40e1f99be8c156cb65a82c98336fb0a435238354
lsamaria <lsamaria@aol.com>
2022-08-10 11:51:31 -0400

    Initial Commit

---
+++ AutoTuneSampler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoTuneSampler
+//
+//  Created by LanceMacBookPro on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
